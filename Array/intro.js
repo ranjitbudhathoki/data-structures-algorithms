b27ae7c506627b8ef6babb2c76e53e6dbe9bc745
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2023-05-30 22:11:27 +0545

    array intro

---
+++ Array/intro.js
@@ -0,0 +1,16 @@
+const strings = ['hello', 'hi', 'welcome']
+// 4*4 = 16 bytes of storage
+
+//push
+strings.push('bye') // O(1)
+
+//pop
+strings.pop() // O(1)
+
+//unshift (add item to the start of the array)
+strings.unshift('x') // O(n)
+
+//shit (delete item from the start of the array)
+strings.shift()
+
+console.log(strings)
