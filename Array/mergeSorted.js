17bfa983c490461360feb004d802d345f4424691
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2023-05-31 20:54:19 +0545

    merge sorted array

---
+++ Array/mergeSorted.js
@@ -0,0 +1,28 @@
+const mergeSortedArray = (arr1, arr2) => {
+  const mergedArray = [];
+  let i = 0; // Pointer for arr1
+  let j = 0; //poin ter for arr2
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+  return mergedArray;
+};
+
+console.log(mergeSortedArray([0, 3, 4, 31], [4, 6, 30]));
