2d1e4ed7ffad97262988a06c5a519c181ac07357
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2023-06-01 09:10:35 +0545

    hash map implementation

---
+++ HashMap/hashmap.js
@@ -0,0 +1,57 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.data[index]) {
+      this.data[index] = [];
+    }
+    this.data[index].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    const bucket = this.data[index];
+    if (bucket.length) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          return bucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          keysArray.push(this.data[i][j][0]);
+        }
+      }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable.set("grapes", 10000));
+console.log(myHashTable.set("apples", 10000));
+
+console.log(myHashTable.get("grapes"));
+
+console.log(myHashTable.keys());
