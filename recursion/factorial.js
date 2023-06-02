55793d2f5f306b78e250bf2defa043148bf52cfb
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2023-06-02 07:23:03 +0545

    recursion

---
+++ recursion/factorial.js
@@ -0,0 +1,17 @@
+function factorial(num) {
+  let total = 1;
+
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+}
+
+// console.log(factorial(4));
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
